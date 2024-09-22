d79b2424f6e770abe18427832e5c7ada177e4ef1
Chandra Bhushan <chandra.bhushan9523@gmail.com>
2024-09-22 14:09:35 +0530

    variable updated

---
+++ variables/variables.py
@@ -0,0 +1,39 @@
+can = "beans"
+print(can)
+
+print(type(can))
+
+pizza = 100
+print(pizza)
+
+print(type(pizza))
+
+samosa = 20.5
+ice_cream = 100
+
+total = pizza + samosa + ice_cream
+
+print(total)
+
+print(type(total))
+
+above_thresold = total > 200
+print(above_thresold)
+
+can = 6.7
+print(type(can))
+
+foo = 'jalebi'
+
+bar = foo
+
+print(bar)
+
+print(id(foo)) 
+
+print(id(bar))
+
+bar = 'samosa'
+print(id(bar))
+
+
