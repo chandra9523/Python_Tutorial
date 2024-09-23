d8089a00a3c2f225155c06a33453c53944ec1029
Chandra Bhushan <chandra.bhushan9523@gmail.com>
2024-09-23 21:03:25 +0530

    numbers concept

---
+++ Numbers/numbers.py
@@ -0,0 +1,71 @@
+base = 10
+height = 7
+
+area = base*height/2
+
+print(area)
+
+print(type(area))
+
+x = 10
+y = 3
+
+print(x + y)
+
+print(x- y)
+print(x*y)
+print(x/y)
+
+print(x//y)
+
+print(x%y)
+
+word = 'Python'
+
+print("PyT" in word)
+
+print(10+2*3)
+
+foo = 2.3e4
+print(foo)
+
+bar = 2.3e-3
+print(bar)
+
+print(1e-3)
+
+food = 120.34
+rent = 500.4
+utilities = 200.5
+total = food + rent+utilities
+print(total)
+
+print(round(total))
+
+income = 10000
+expenses = 9000
+saving = income - expenses
+print(saving)
+
+food = "120.34"
+rent = "500.67"
+
+total = float(food)+float(rent)
+
+print(total)
+
+import math
+
+print(math.sqrt(16))
+
+print(math.floor(3.7))
+
+print(math.ceil(3.7))
+
+print(math.pi)
+
+print(dir(math))
+
+print(format(5,'b'))
+
+print(format(9,'b'))
